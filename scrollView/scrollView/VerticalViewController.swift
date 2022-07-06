98f856e9986b8638ad038a7b39dd270cc9a43d17
yeojinyun <sail041@naver.com>
2022-07-06 19:50:53 +0900

    add project

---
+++ scrollView/scrollView/VerticalViewController.swift
@@ -0,0 +1,128 @@
+//
+//  VerticalViewController.swift
+//  scrollView
+//
+//  Created by 순진이 on 2022/07/05.
+//
+import UIKit
+import SnapKit
+
+class VerticalViewController : UIViewController {
+ 
+    
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    let firstLabel = UILabel()
+    let secondLabel = UILabel()
+    let thirdLabel = UILabel()
+    let firstImageView = UIImageView()
+    let secondImageView = UIImageView()
+    let thirdImageView = UIImageView()
+    let lastButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setUI()
+    }
+    @objc func buttonTapped(_ sender: UIButton) {
+        let scrollVC = ScrollViewController()
+        self.navigationController?.pushViewController(scrollVC, animated: true)
+    }
+}
+
+// MARK: - SetUI
+extension VerticalViewController {
+    final private func setUI() {
+        setDetails()
+        setLayout()
+        addTarget()
+    }
+
+    final private func setDetails() {
+        firstLabel.text = "측면 뭉치"
+        secondLabel.text = "정면 뭉치"
+        thirdLabel.text = "서 있는 뭉치"
+        
+        [firstImageView, secondImageView, thirdImageView].forEach {
+            $0.contentMode = .scaleAspectFit
+            firstImageView.image = UIImage(named: "뭉치1")
+            secondImageView.image = UIImage(named: "뭉치2")
+            thirdImageView.image = UIImage(named: "뭉치3")
+        }
+        
+        lastButton.setTitle("버튼입니다.", for: .normal)
+        lastButton.setTitleColor(.white, for: .normal)
+        lastButton.backgroundColor = .black
+    }
+    
+    final private func addTarget() {
+        lastButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    }
+    
+    final private func setLayout() {
+        [scrollView].forEach {
+            view.addSubview($0)
+        }
+
+
+        scrollView.snp.makeConstraints {
+            $0.leading.top.trailing.bottom.equalToSuperview()
+        }
+        
+        scrollView.addSubview(contentView)
+
+        [firstLabel, secondLabel, thirdLabel, firstImageView, secondImageView, thirdImageView, lastButton].forEach {
+            contentView.addSubview($0)
+        }
+
+        
+        contentView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView.contentLayoutGuide)
+            $0.width.equalTo(scrollView.frameLayoutGuide)
+            $0.height.equalTo(1200)
+        }
+        
+        firstLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(contentView).offset(50)
+        }
+        
+        firstImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(firstLabel.snp.bottom).offset(15)
+            make.height.equalTo(300)
+            make.leading.trailing.equalTo(contentView)
+        }
+        
+        secondLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(firstImageView.snp.bottom).offset(50)
+        }
+        
+        secondImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(secondLabel.snp.bottom).offset(15)
+            make.height.equalTo(300)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        thirdLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(secondImageView.snp.bottom).offset(50)
+        }
+        
+        thirdImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(thirdLabel.snp.bottom).offset(15)
+            make.height.equalTo(300)
+            make.leading.trailing.equalToSuperview()
+        }
+        lastButton.snp.makeConstraints { make in
+//            make.top.equalTo(thirdImageView.snp.bottom).offset(15)
+            make.bottom.equalTo(contentView)
+            make.leading.trailing.equalToSuperview().inset(20)
+            
+        }
+    }
+}
---
+++ scrollView/scrollView/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  scrollView
+//
+//  Created by 순진이 on 2022/07/05.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController : UIViewController {
+    let verticalButton = UIButton()
+    let horizontalButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setDetail()
+    }
+    
+    @objc func verticalBtnTapped(_ sender: UIButton) {
+        let nextVC = VerticalViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @objc func horizontalBtnTapped(_ sender: UIButton) {
+        let nextVC = HorizontalViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+}
+
+extension ViewController {
+    func setUI() {
+        [verticalButton, horizontalButton].forEach {
+            view.addSubview($0)
+        }
+        
+        verticalButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(400)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        horizontalButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(verticalButton.snp.bottom).offset(50)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+    }
+    
+    func setDetail() {
+        
+        [verticalButton, horizontalButton].forEach {
+            $0.setTitleColor(.white, for: .normal)
+            verticalButton.backgroundColor = .black
+            verticalButton.addTarget(self, action: #selector(verticalBtnTapped(_:)), for: .touchUpInside)
+            verticalButton.setTitle("Vertical", for: .normal)
+            horizontalButton.backgroundColor = .blue
+            horizontalButton.addTarget(self, action: #selector(horizontalBtnTapped(_:)), for: .touchUpInside)
+            horizontalButton.setTitle("horizontal", for: .normal)
+        }
+    }
+}

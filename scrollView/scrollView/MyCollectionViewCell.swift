9571d51dd77672e50912672f37e31e03ce1e572c
yeojinyun <sail041@naver.com>
2022-08-07 23:24:58 +0900

    scrollView

--- scrollView/scrollView/MyCollectionViewCell.swift
+++ scrollView/scrollView/MyCollectionViewCell.swift
@@ -9,22 +9,53 @@ import UIKit
 
 class MyCollectionViewCell: UICollectionViewCell {
     
+    let scrollView = UIScrollView()
     let imageView = UIImageView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setScrollView()
         setConstraints()
+        setDoubleTap()
+        scrollView.backgroundColor = .red
         imageView.backgroundColor = .yellow
+    }
+    
+    func setDoubleTap() {
+        let doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(doubleTapped(_:)))
+        doubleTapGesture.numberOfTapsRequired = 2
+        addGestureRecognizer(doubleTapGesture)
+    }
+    
+    @objc func doubleTapped(_ gestureRecognizer: UITapGestureRecognizer) {
+        
+    }
+    
+    func setScrollView() {
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 4.0
     }
 
     func setConstraints() {
-        contentView.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+        [scrollView, imageView].forEach {
+            contentView.addSubview(scrollView)
+            scrollView.addSubview(imageView)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
         NSLayoutConstraint.activate([
-            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            scrollView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            imageView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            imageView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            imageView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+            imageView.heightAnchor.constraint(equalTo: scrollView.frameLayoutGuide.heightAnchor)
         ])
     }
     
@@ -36,5 +67,11 @@ class MyCollectionViewCell: UICollectionViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MyCollectionViewCell: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
